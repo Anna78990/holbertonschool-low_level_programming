3cba2319a0dccb9f35a3d31f151324b8ca096e13
vagrant <vagrant@vagrant.vm>
2021-10-01 14:34:54 +0000

    l

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - This function print all of the number between 0 and 9.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+	for (n = '0' ; n <= '9' ; n++)
+	{
+	putchar(i);
+	putchar(n);
+	if (i == '9' && n == '9')
+	{
+	continue;
+	}
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
