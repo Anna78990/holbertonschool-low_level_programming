e7fd79a8b33eb4287e759480c5bf6c7e788437e1
vagrant <vagrant@vagrant.vm>
2021-10-01 11:16:04 +0000

    d

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,10 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - This function is to find if the number is positive or negative.
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -13,14 +16,14 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	printf("%d is positive.\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero.\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
-	{printf("%d is negative.\n", n);
+	{printf("%d is negative\n", n);
 	};
 
 	return (0);
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/* This function is used to compare the last number to 5.*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("Last digit of %d", n);
+	if ((n % 10) > 5)
+	{
+	printf(" is %d and and is greater than 5.\n", n % 10);
+	}
+	else if ((n % 10) == 0)
+	{
+	printf(" is %d and and is 0.\n", n % 10);
+	}
+	else
+	{
+	printf(" is %d and is less than 6 and not 0.\n", n % 10);
+	}
+
+	return (0);
+}
