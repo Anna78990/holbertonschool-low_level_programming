52e41aaeba4fc1ebf5633c5008cdc25f753d46d3
vagrant <vagrant@vagrant.vm>
2021-10-01 12:54:36 +0000

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - This function print all of the number between 0 and 9.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+	printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
