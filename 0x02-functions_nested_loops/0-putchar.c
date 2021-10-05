151c3ec28e5a558663f9b1dac9614905c8b5ca9d
vagrant <vagrant@vagrant.vm>
2021-10-05 11:45:15 +0000

    f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,13 @@
 */
 	int main(void)
 	{
-	char *str ="_putchar";
+	char *str = "_putchar";
 	int i;
 
 	for (i = 0; i < 9; i++)
 	{
 	putchar(str[i]);
- 	}
- 	putchar('\n');
-	return (0);
 	}
-         
+	putchar('\n');
+	return (0);
+	} 
