990b25f4926740a743854fa48383eb1f04d6468c
vagrant <vagrant@vagrant.vm>
2021-10-06 19:47:13 +0000

    m

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,24 +1,12 @@
 #include "main.h"
-#include <math.h>
 
-/* more headers goes there */
 /**
-* jack_bauer - return the time
-* Return: Nothing
-*/
-void jack_bauer(void)
-	{
-	int h;
-	int m;
-
-	for (h = 0 ; h < 24; h++)
-	{
-		_putchar(h / 10 + '0');
-		_putchar(h % 10 + '0');
-
-		for (m = 0 ; m <= 60 ; m++)
-		{
-		_putchar(m / 10 + '0');
-		_putchar(m % 10 + '0');
-		}
-	}
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
--- 0x02-functions_nested_loops/a.c
+++ 0x02-functions_nested_loops/a.c
@@ -13,14 +13,12 @@ void jack_bauer(void)
 
 	for (h = 0 ; h < 24; h++)
 	{
-		for (m = 0 ; m < 60 ; m++)
-		{
 		_putchar(h / 10 + '0');
 		_putchar(h % 10 + '0');
-		_putchar(':');
+
+		for (m = 0 ; m <= 60 ; m++)
+		{
 		_putchar(m / 10 + '0');
 		_putchar(m % 10 + '0');
-		_putchar('\n');
 		}
-	}
 	}
