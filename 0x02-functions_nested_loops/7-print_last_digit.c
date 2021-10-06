0c4057b1c0e85ce611fc17e4bf0d5ab9041a577d
vagrant <vagrant@vagrant.vm>
2021-10-06 17:12:27 +0000

    l

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdlib.h>
+#include "main.h"
+
+/* more headers goes there */
+/**
+* print_last_digit - return the absolute value of last digit
+* @a : integer
+* Return: (abs(a % 10))
+*/
+int print_last_digit(int a)
+	{
+	return (abs(a % 10));
+	}
