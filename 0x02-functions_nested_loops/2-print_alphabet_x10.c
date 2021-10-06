9dda0ee6a902b4743be25e9bb8b70dd95955e32d
vagrant <vagrant@vagrant.vm>
2021-10-06 09:21:00 +0000

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,30 @@
 
 /* more headers goes there */
 /**
-* print_alphabet - print all of the lower case alphabets
+* print_alphabet_x10 - print all of the lower case alphabets x10
 * Return: 0
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 	{
-	int ch;
 	int i;
-	for (i = 0; i <10 ; i++)
+
+	for (i = 0; i < 10; i++)
 	{
-		for (ch = 'a' ; ch <= 'z' ; ch++)
-		{
-		_putchar(ch);
-		}
+	print_alphabet();
+	}
+	}
+
+/**
+* print_alphabet - print all of the lower case alphabets
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	_putchar(ch);
+	}
 	_putchar('\n');
-	}
-	}
+}
