b1b45d4a6a6b9746eec7781e7130e104ce98511a
vagrant <vagrant@vagrant.vm>
2021-10-07 20:28:46 +0000

    l

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,37 +12,15 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0)))
+	if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100!= 0)))
 	{
-	if (month >= 2 && day >= 60)
-	{
-	day++;
-	}
-	if ((month == 2) && (day >= 60))
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-	}
-	else if ((month == 4) && (day >= 121))
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 90, year);
-	}
-	else if ((month == 6) && (day >= 182))
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 151, year);
-	}
-	else if ((month == 9) && (day >= 274))
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 243, year);
-	}
-	else if ((month == 11) && (day >= 335))
-	{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 304, year);
-	}
-	else
-	{
+		if (month >= 2 && day >= 60)
+		{
+		day++;
+		}
+
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
-	}
 	}
 	else
 	{
