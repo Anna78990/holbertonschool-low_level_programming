871cb188b0b65aca27f4c9e9a0b1b4b66da748e9
vagrant <vagrant@vagrant.vm>
2021-10-08 10:48:10 +0000

    m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * mul - it multiplies two integers.
+ * @a: integer
+ * @b: integer
+ * Return: (k)
+ */
+
+int mul(int a, int b)
+{
+int k = a * b;
+return (k);
+}
