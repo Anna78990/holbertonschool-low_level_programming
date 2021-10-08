bd4ba0af167539154f11bfa6f2c8a49b89867ce9
vagrant <vagrant@vagrant.vm>
2021-10-08 10:10:30 +0000

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - it checks for uppercase character.
+ * @c: third integer
+ * Return: (1), (0)
+ */
+ 
+int _isupper(int c)
+{
+if (isupper(c)){
+return (1);
+}
+else
+{
+return (0);
+}
+}
