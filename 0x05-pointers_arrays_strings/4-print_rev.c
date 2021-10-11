367577d14993d61c96b7b33f12d4e50350020b8f
vagrant <vagrant@vagrant.vm>
2021-10-11 14:45:29 +0000

    l

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_rev - prints a string, in reverse, followed by a new line.
+* @s: pointer
+* Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	
+	for (i = strlen(s) ; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
