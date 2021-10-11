26abb6f2a6d8790ef4618af8ca0e5e94ec26c0fa
vagrant <vagrant@vagrant.vm>
2021-10-11 17:20:19 +0000

    m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* rev_string - reverses a string.
+* @s: pointer
+* Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = (strlen(s) - 1) ; i >= 0 ; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
