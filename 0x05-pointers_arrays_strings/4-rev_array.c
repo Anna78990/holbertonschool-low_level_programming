bd82f4dae6d50924e712e2338ae934c7f5d99967
vagrant <vagrant@vagrant.vm>
2021-10-14 07:59:18 +0000

    m

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+* reverse_array - reverses the content of an array of integers.
+* @a: array
+* @n: number of arrays
+* Return: Nothing
+*/
+
+void reverse_array(int *a, int n)
+{
+
+}
