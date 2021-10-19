1988cb39314825cea0515c7fe234d65a855ea7de
vagrant <vagrant@vagrant.vm>
2021-10-19 09:07:57 +0000

    task3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,7 @@
  * *_strchr - locates a character in a string
  * @s: pointer to the string
  * @c: character to search
- * Return: (*s)
+ * Return: (&s[i])(NULL)
  */
 
 char *_strchr(char *s, char c)
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,32 +5,14 @@
 /**
  * *_strchr - locates a character in a string
  * @s: pointer to the string
- * @c: character to search
- * Return: (*s)
+ * @accept: character to search
+ * Return: (n)
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-int len;
-int i;
-int n;
-char *ch;
+	unsigned int n;
 
-len = strlen(s);
-	for (i = 0; i < len; i++)
-	{
-	if (s[i] == c)
-	{
-		for (n = i; n < len; n++)
-		{
-		ch = s;
-		}
-		return (*ch);
-		break;
-	}
-	else
-	{
-	return (NULL);
-	}
-	}
+	n = strspn(s, accept);
+	return (n);
 }
