3e0b371c320fcf35528e4a92af83c9e166c452a1
vagrant <vagrant@vagrant.vm>
2021-10-19 11:14:24 +0000

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,18 +1,33 @@
 #include "main.h"
-#include <string.h>
 #include <stdlib.h>
 
 /**
- * _strspn - locates a character in a string
+ * *_strpbrk - searches a string for any of a set of bytes.
  * @s: pointer to the string
  * @accept: character to search
- * Return: (n)
+ * Return: (&s[i])(NULL)
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int n;
+	int len;
+	int i;
+	int n;
+	int lena;
 
-	n = strspn(s, accept);
-	return (n);
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	for (lena = 0; accept[lena] != '\0'; lena++)
+		;
+	for (i = 0; i <= len; i++)
+	{
+		for (n = 0; n <= lena; n++)
+		{
+			if (s[i] == accept[n])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
 }
