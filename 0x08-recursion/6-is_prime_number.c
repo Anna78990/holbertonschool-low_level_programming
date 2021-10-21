292b0d32113224d19e65519cb655b399c85a6324
vagrant <vagrant@vagrant.vm>
2021-10-21 13:26:03 +0000

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,33 +10,33 @@ int is_prime_number(int n)
 {
 	if ((n != 2) && (n % 2) == 0)
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return prime(2, n);
+		return (prime(2, n));
 	}
 }
 
-int prime (int i, int N)
+int prime(int i, int N)
 {
 	if (i <= N)
 	{
 		if (N % i == 0 && N != i)
 		{
-			return 0;
+			return (0);
 		}
 		else if (N % i == 0 && N == i)
 		{
-			return 1;
+			return (1);
 		}
 		else
 		{
-			return prime(i + 1, N);
+			return (prime(i + 1, N));
 		}
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
