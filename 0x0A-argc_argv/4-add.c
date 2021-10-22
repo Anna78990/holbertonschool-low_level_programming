cbd3a15da380cee2d4f6440874743922eb875b15
vagrant <vagrant@vagrant.vm>
2021-10-22 12:25:19 +0000

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,10 +3,10 @@
 #include <ctype.h>
 
 /**
- * main - main function
+ * main - adds positive numbers.
  * @argc: argc
  * @argv: argv
- * Return: (0)
+ * Return: (0)(1)
  */
 
 int main(int argc, char *argv[])
@@ -23,20 +23,20 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 	for (; argv[k]; k++)
 	{
 		for (; argv[k][i]; i++)
+		{
+			if ((isdigit(argv[k][i])) == 0)
 			{
-				if ((isdigit(argv[k][i])) == 0)
-				{
-					printf("Error\n");
-					return 1;
-				}
+				printf("Error\n");
+				return (1);
 			}
+		}
 		ad += atoi(argv[k]);
 	}
-	printf("%d\n",ad);
-	return 0;
+	printf("%d\n", ad);
+	return (0);
 }
