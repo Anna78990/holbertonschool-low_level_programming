2254e34554a2d1872d36e117b30cc3b7889cd582
vagrant <vagrant@vagrant.vm>
2021-10-22 12:05:06 +0000

    task 3 modified

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,14 +5,18 @@
  * main - that multiplies two numbers
  * @argc: argc
  * @argv: argv
- * Return: (0)
+ * Return: (1)(0)
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
 	(void) argc;
-
+	if (!(argv[1]) || !(argv[2]))
+	{
+	printf("Error\n");
+	return (1);
+	}
 	i = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", i);
 	return (0);
