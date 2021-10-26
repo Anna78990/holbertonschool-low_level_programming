1bbe6a52770e83861275fe4dc5f60b48306d50a8
vagrant <vagrant@vagrant.vm>
2021-10-26 09:18:23 +0000

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *ch;
 	unsigned int i;
 
-	ch = malloc(size * sizeof(char));
 	if (size == 0)
 	{
 	return (NULL);
 	}
 	else
 	{
+	ch = malloc(size * sizeof(char));
 		for (i = 0; i <= size; i++)
 		{
 			ch[i] = c;
