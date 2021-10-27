a3cfa0589389c7e7ebf05dd8da9cf5ff2a93046c
vagrant <vagrant@vagrant.vm>
2021-10-27 14:28:03 +0000

    modi

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,14 +3,14 @@
 
 /**
  * free_grid - free the memmory of alloc_grid
- * @grid - pointer to pointer
- * @height - pointer to width
+ * @grid: pointer to pointer
+ * @height: pointer to width
  */
 
 void free_grid(int **grid, int height)
 {
 	int j;
-	
+
 	for (j = 0; j < height; j++)
 	{
 		free(grid[j]);
