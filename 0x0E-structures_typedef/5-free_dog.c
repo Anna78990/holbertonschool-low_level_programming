50bf4126b95a8261817d50c5354db34f86b4befe
vagrant <vagrant@vagrant.vm>
2021-11-02 16:00:45 +0000

    task4

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,15 @@
-#include "main.h"
+#include "dog.h"
 #include <stdlib.h>
 
 /**
- * free_grid - free the memmory of alloc_grid
- * @grid: pointer to pointer
- * @height: pointer to width
+ * free_dog - free the memmory of structure
+ * @d: pointer to pointer
+ * Return: Nothing
  */
 
 void free_dog(dog_t *d)
 {
-	int j;
-
-	for (j = 0; j < height; j++)
-	{
-		free(grid[j]);
-	}
-	free(grid);
+	free((*d).name);
+	free((*d).owner);
+	free(d);
 }
