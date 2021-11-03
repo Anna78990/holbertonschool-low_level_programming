9c309e95f868ad5cc06c3ab6f1d76a80c415cc8f
vagrant <vagrant@vagrant.vm>
2021-11-03 13:22:42 +0000

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print an array
+ * @array: an array to print
+ * @size: the size of array to print
+ * @action: pointer to the function
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
