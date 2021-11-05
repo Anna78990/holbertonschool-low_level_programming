72b268c9066842cdb44bc348cdeed7bde08a4318
vagrant <vagrant@vagrant.vm>
2021-11-05 16:34:10 +0000

    task2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - print number
+ * print_strings - print number
  * @separator: separator
  * @n: value of variadic argument
  */
@@ -11,6 +11,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
+char *p;
 va_list ap;
 va_start(ap, n);
 
@@ -20,11 +21,14 @@ for (i = 0; i < n; i++)
 	printf("%s", va_arg(ap, char *));
 }
 else
-for (i = 0; i < n; i++)
 {
 for (i = 0; i < n; i++)
 {
-	printf("%s", va_arg(ap, char *));
+	p = va_arg(ap, char *);
+	if (p != NULL)
+		printf("%s", p);
+	else if (p == NULL && i != (n - 1))
+		printf("(nil)");
 	if (i != (n - 1))
 		printf("%s", separator);
 }
