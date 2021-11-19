8b6d4a3ba226d9bc2cf3da5bebd43e23a142288e
Anna78990 <takasuanna1990@gmail.com>
2021-11-19 15:00:57 +0000

    if the last character was 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: binary character chaine
+ * Return: sum ou binary
+*/
+
 
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n, z, sum = 0;
 	unsigned int i = 1;
-	
+
 	if (b == NULL)
 		return (0);
 	for (n = 0; b[n]; n++)
@@ -14,13 +20,20 @@ unsigned int binary_to_uint(const char *b)
 	if (n > 0)
 		i *= 2;
 	}
-	for (z = 0; b[z]; z++)
+	for (z = 0; z <= n; z++)
 	{
 		if (b[z] != '0' && b[z] != '1')
 			return (0);
+		else if (z == (n - 1))
+		{
+			if (b[z] == '1')
+				i += 1;
+			else
+				continue;
+		}
 		else if (b[z] == '1')
 			sum += i;
-		else 
+		else
 			sum += 0;
 	i /= 2;
 	}
