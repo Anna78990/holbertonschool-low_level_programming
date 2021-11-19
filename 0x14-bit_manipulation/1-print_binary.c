c3f019f964eb2e90c35adb80e360bf507d1ea1f3
Anna78990 <takasuanna1990@gmail.com>
2021-11-19 15:21:12 +0000

    for betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_binary - print the binary number by the decimal number
+ * @n: the decimal number to convert
+ * Return: Nothing
+ */
+
 void print_binary(unsigned long int n)
 {
-
 	if (n == 0)
 		putchar('0');
 	else if (n == 1)
@@ -14,13 +19,18 @@ void print_binary(unsigned long int n)
 	}
 }
 
+/**
+ * binary_rec - print the binary number from the reverse
+ * @n: the number to print
+ * Return: Nothing
+ */
 void binary_rec(unsigned long int n)
 {
 	if (n == 0)
 		return;
 	if (n > 0)
 		binary_rec(n >> 1);
-	if((n & 1) == 1)
+	if ((n & 1) == 1)
 		_putchar('1');
 	else
 		_putchar('0');
