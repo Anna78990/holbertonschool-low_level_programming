7b31b578bc72ba9a53f59e18b90ad12e4f3006bd
Anna78990 <takasuanna1990@gmail.com>
2021-11-23 09:05:16 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,29 +4,33 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it.
- * @filename : filename to rewrite
- * @letters : text written in the file
- * Return: the actual number of letters, or 0 otherwise.
+ * create_file - creates a file.
+ * @filename : filename to create
+ * @text_content: text written in the file
+ * Return: if its succeeded 1,otherwise -1.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-int fd, rd;
+int fd, i, z, wt;
 char *buf;
-size_t wt;
-
-buf = (char *)malloc(letters);
 
 if (filename == NULL)
 	return (-1);
-fd = open(file, O_RDWD|O_CREAT|O_TRUNC, 0666);
+if (text_content != NULL)
+{
+	for (i = 0; text_content[i]; i++)
+		;
+	buf  = (char *)malloc(i);
+	for (z = 0; z <= i; z++)
+		buf[z] = text_content[z];
+}
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (fd < 0)
-	return (0);
-rd = read(fd, buf, letters);
-wt = write(STDOUT_FILENO, buf, letters);
-if (wt < letters)
-	return (0);
+	return (-1);
+wt = write(fd, buf, i);
+if (wt < 0)
+	return (-1);
 close(fd);
-return (rd);
+return (1);
 }
