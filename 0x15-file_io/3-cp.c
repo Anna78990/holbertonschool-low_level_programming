d2a6b4f70099cd2d5a65b8c57aa0bbcafd84ab39
Anna78990 <takasuanna1990@gmail.com>
2021-11-24 15:25:34 +0000

    permission write

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int ac, char **av)
 	fdr = open(av[1], O_RDONLY);
 	if (fdr == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fdw = open(av[2], O_RDONLY | O_CREAT | O_TRUNC, 0664);
+	fdw = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fdw == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
 	while ((rd = read(fdr, buf, 1024)) > 0)
