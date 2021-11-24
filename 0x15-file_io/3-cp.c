f36bb1101c0a08723166849b5be0f6e5ec917226
Anna78990 <takasuanna1990@gmail.com>
2021-11-24 15:08:14 +0000

    Incitatous

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,43 +13,27 @@
 int main(int ac, char **av)
 {
 	int fdr, fdw, rd, wt, clr, clw;
-	char buf[1024];
+	char buf[5000];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fdr = open(av[1], O_RDONLY);
 	if (fdr < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	rd = read(fdr, buf, 1024);
-	if (rd < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fdw = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	wt = write(fdw, buf, rd);
-	if (fdw < 0 || wt < 0)
+	while ((rd = read(fdr, buf, 1024)) > 0)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", av[2]);
-		exit(99);
+		wt = write(fdw, buf, rd);
+		if (fdw < 0 || wt < 0)
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
 	}
+	if (rd < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	clr = close(fdr);
 	if (clr < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fdr);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdr), exit(100);
 	clw = close(fdw);
 	if (clw < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fdw);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", fdw), exit(100);
 	return (1);
 }
---
+++ 0x15-file_io/test.c
@@ -0,0 +1,36 @@
+#include "test.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void _puts_recursion(char *s)
+{
+	if(s != NULL)
+	{
+		_puts_recursion
+		pu
+	}
+}
