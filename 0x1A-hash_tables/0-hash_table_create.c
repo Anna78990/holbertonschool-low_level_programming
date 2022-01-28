3aa51255ae81c2345dc4d0b6afa20bfd68ef6569
Anna78990 <takasuanna1990@gmail.com>
2022-01-28 11:08:53 +0000

    1, 2

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *p;
+
+	if (size)
+	{
+		p = (hash_table_t *)malloc(sizeof(hash_table_t));
+		if (p == NULL)
+			return NULL;
+		else
+		{
+			p->size = size;
+			p->array = NULL;
+			return (p);
+		}
+	}
+	else
+		return NULL;
+}
